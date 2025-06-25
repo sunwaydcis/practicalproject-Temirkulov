0ea08edef23368c2cdc640c59c463db872f490ae
Mirkamol Temirkulov <mirkosht1@gmail.com>
2025-06-25 16:15:04 +0800

    Connected Controller to Scenebuilder
    connected Controller variables to tableview elemenets within Scenebuilder.

--- src/main/scala-3/ch/makery/address/MainApp.scala
+++ src/main/scala-3/ch/makery/address/MainApp.scala
@@ -1,16 +1,37 @@
 package ch.makery.address
 
+import ch.makery.address.model.Person
 import javafx.fxml.FXMLLoader
 import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.scene.Scene
 import scalafx.Includes.*
 import javafx.scene as jfxs
+import scalafx.beans.property.StringProperty
+import scalafx.collections.ObservableBuffer
 
 object MainApp extends JFXApp3:
 
   //Window Root Pane
   var roots: Option[scalafx.scene.layout.BorderPane] = None
+  /**
+   * The data as an observable list of Persons.
+   */
+  val personData = new ObservableBuffer[Person]()
+
+  /**
+   * Constructor
+   */
+  personData += new Person("Hans", "Muster")
+  personData += new Person("Ruth", "Mueller")
+  personData += new Person("Heinz", "Kurz")
+  personData += new Person("Cornelia", "Meier")
+  personData += new Person("Werner", "Meyer")
+  personData += new Person("Lydia", "Kunz")
+  personData += new Person("Anna", "Best")
+  personData += new Person("Stefan", "Meier")
+  personData += new Person("Martin", "Mueller")
+
 
   override def start(): Unit =
     // transform path of RootLayout.fxml to URI for resource location.
@@ -37,3 +58,24 @@ object MainApp extends JFXApp3:
     loader.load()
     val roots = loader.getRoot[jfxs.layout.AnchorPane]
     this.roots.get.center = roots
+
+  val string1 = new StringProperty("hello") //publisher
+  val string2 = new StringProperty("sunway") //subscriber
+  val string3 = new StringProperty("segi") //subscriber
+
+  string1.onChange((a, b, c) => {
+    println("string1 has changed")
+  })
+  string1.onChange((a, b, c) => {
+    println("string1 has changed from " + b + " to " + c)
+  })
+  string1.value = "taylor"
+  string2.value = "monash"
+  string3.value = "inti"
+  string2 <==> string1
+  string3 <== string1
+
+  println(string1.value)
+  println(string2.value)
+  println(string3.value)
+end MainApp
